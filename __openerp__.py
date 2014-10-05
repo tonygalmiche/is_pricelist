1c8eca74111d8a9f3ffccbbbabc452bd98d3e349
Tony Galmiche <tony.galmiche@infosaone.com>
2014-10-05 10:59:20 +0200

    Mise en ligne

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+	"name" : "InfoSaône - Liste de prix",
+	"version" : "0.1",
+	"author" : "InfoSaône",
+	"category" : "InfoSaône",
+
+
+   'description': """
+InfoSaône / Module permettant d'ajouter des fonctionnalés aux liste de prix : 
+- Ajout d'un menu pour importer les item des versions des listes de prix
+
+===================================================
+""",
+    'maintainer': 'InfoSaône',
+    'website': 'http://www.infosaone.com',
+
+	"depends" : ["purchase"],
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["is_pricelist.xml"],
+	"installable": True,
+	"active": False
+}
+
+
+
+
